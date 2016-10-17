bea9537d9b89817799ec1fcccf31b297ec36f55c
umair-khanzada <umairkz52@gmail.com>
2016-10-18 01:38:12 +0500

    refactor code.

--- public/app.js
+++ public/app.js
@@ -7,21 +7,22 @@ angular.module('serveBox', ['ngCookies'])
     .controller('appCtrl', function($scope, $http, $cookies){
 
         //Initialization.
-        $scope.user = {email: '', password: ''};
-        $scope.isLoggedIn =  !!$cookies.get('Token');
+        $scope.user = {email: '', password: ''};        //user obj.
+        $scope.isLoggedIn =  !!$cookies.get('Token');       //check user is login.
+
         $scope.login = function(form){
             if(form.$valid){
                 $http.post('/login ', $scope.user).then(function(res){
-                        $cookies.put('Token', res.data.token);
-                        window.location.assign("/");
+                        $cookies.put('Token', res.data.token);      //put token in cookies.
+                        window.location.assign("/");        //redirect to main page after login.
                 },
                 function(err){
-                    console.log("err", err)
+                    $scope.error = err.data;
                 });
             }
         };
 
         $scope.logout = function(){
-            $cookies.remove('Token');
+            $cookies.remove('Token');       //remove token from cookies.
         }
     });
--- server.js
+++ server.js
@@ -2,41 +2,51 @@
  * Created by Owais on 10/16/2016.
  */
 
-var express    =    require('express');
+var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var app        =    express();
+var app = express();
 var users = [{name: "umair", email: "umair@gmail.com", password: "123"}];
-var isLogin = function (req, res, next) {
+
+//middleware function, check user is login.
+function isLogin(req, res, next) {
     if(Number(new Date()) > req.cookies.token || !req.cookies.token) res.redirect('/login');
     else next();
-};
+}
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
 
-app.get('/',function(req, res){
+app.use(express.static(__dirname + '/public'));     //serve static files.
+app.use(bodyParser.json());     //getting data from req.body
+app.use(cookieParser());        //get and set data in cookies.
+
+
+
+app.get('/', isLogin, function(req, res){
     res.render('index.html')
 });
+
 app.get('/about', isLogin, function(req, res){
     res.render('about.html')
 });
+
 app.get('/contact', isLogin, function(req, res){
     res.render('contact.html')
 });
+
 app.get('/login',function(req, res){
     res.render('login.html')
 });
+
 app.get('/logout',function(req, res){
     res.clearCookie('token');
     res.render('login.html')
 });
+
 app.post('/login',function(req, res){
     var user = users.filter(function(obj){
         return req.body.email === obj.email && req.body.password === obj.password;
     });
+
     if(user.length){
         var token = new Date().setTime(new Date().getTime() + (2 * 60 * 1000));
         res.cookie('token', token, {httpOnly: true });
@@ -45,9 +55,9 @@ app.post('/login',function(req, res){
     else res.status(404).send('Incorrect email and password combination.');
 });
 
-app.set('views',__dirname + '/views');
+app.set('views',__dirname + '/views');      //template path.
 app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+app.engine('html', require('ejs').renderFile);      //for rendering html file.
 
 var server     =    app.listen(4000, function(){
     console.log("We have started our server on port 4000");
